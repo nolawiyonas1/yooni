8c24ae630cec5af2b66f93c2f50897006d885458
Ifeoluwa Oyetimehin <ifeoluwa.oyetimehin@gmail.com>
2026-02-14 18:27:54 -0800

    Update App UI

--- android/app/src/main/java/com/example/yooni/ui/theme/Color.kt
+++ android/app/src/main/java/com/example/yooni/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Yooni brand gradient and text
+val YooniPink = Color(0xFFF8BBD9)
+val YooniBlue = Color(0xFFB3E5FC)
+val YooniTextDark = Color(0xFF424242)
